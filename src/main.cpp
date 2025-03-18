6f828198b82dd21e7f40f277fca888ec0a43b41b
K-Jacko <wakumx@icloud.com>
2025-03-18 02:52:36 +0000

    Deletion of old Engine

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-#include "Game.h"
-Game* game = nullptr;
 
 int main(int argc, char* args[]) {
-    SDL_SetMainReady();
-    game = new Game();
-    game->Init();
-    while(game->isRunning())
-    {
-        game->Event();
-        game->Update();
-        game->Draw();
-    }
+
     return 0;
 }
